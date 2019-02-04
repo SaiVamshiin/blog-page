cc5230583ec0c1d6960c6ba40b03d6d5b706a206
Lucas159 <rigal.lucas@gmail.com>
2019-02-04 14:57:21 +0100

    ajout login + structure index

--- index.php
+++ index.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>blog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
-    
+    <h1>Blog-Talis</h1>
+    <section id="welcome">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Les question ?</h2>
+                </div>
+                <div class="col-md-6">
+                    <h2>Mes messages</h2>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
