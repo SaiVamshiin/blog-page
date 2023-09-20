e2b03214c5caaa30717ed938da33b3b2902d531b
B Sai Vamshi <134629654+SaiVamshiin@users.noreply.github.com>
2023-09-20 19:24:43 +0545

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
     <form action="login_post.php" method="post">
         <input type="text" placeholder="pseudo" name="pseudo"> 
         <input type="password" placeholder="password" name="password">
-        <input type="submit" value="Connexion">
+        <input type="submit" value="Connexion"> 
     </form>
     <?php
     if(isset($_GET["message"])){ 
